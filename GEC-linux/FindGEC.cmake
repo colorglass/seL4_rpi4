caed716b43e70d832ae2d1d719b1cb04b06c9d68
colored-dye <baoyuntai@outlook.com>
2023-04-16 14:28:20 +0800

    TODO: GEC blocking

--- GEC-linux/FindGEC.cmake
+++ GEC-linux/FindGEC.cmake
@@ -1,6 +1,1 @@
-set(GEC_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE STRING "")
-mark_as_advanced(GEC_DIR)
-
-macro(GEC_import_library)
-    add_subdirectory(${GEC_DIR} GEC)
-endmacro()
+../GEC/FindGEC.cmake
