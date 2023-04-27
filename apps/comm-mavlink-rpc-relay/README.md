c1952238a4856ce65131c96aaa48073f08b39081
colored-dye <baoyuntai@outlook.com>
2023-04-27 14:37:23 +0800

    Added comm-mavlink-rpc-relay

---
+++ apps/comm-mavlink-rpc-relay/README.md
@@ -0,0 +1,5 @@
+Raspberry Pi 4B relay plaintext messages between GCS and Pixhawk.
+
+Utilizing SMP and RPC calls.
+
+Pixhawk [Telem 2]    <===[serial wire]===>     [UART 5] Raspberry Pi [UART 3, 3DR Radio Telemetry]    <===[915 MHz wireless]===>    [3DR Radio Telemetry] Laptop.
---
+++ apps/comm-mavlink-rpc-relay/components/Encrypt/src/Encrypt.c
@@ -0,0 +1,64 @@
+#include <camkes.h>
+#include <camkes/io.h>
+#include <platsupport/chardev.h>
+#include <stdint.h>
+
+#include "mavlink/v2.0/ardupilotmega/mavlink.h"
+#include "mavlink/v2.0/common/mavlink.h"
+#include "mavlink/v2.0/mavlink_helpers.h"
+#include "mavlink/v2.0/mavlink_types.h"
+#include "my_type.h"
+
+static mavlink_message_t mavlink_message_rx_buffer;
+static mavlink_status_t mavlink_status;
+
+static uint8_t my_mavlink_parse_char(uint8_t c, mavlink_message_t *r_message,
+                                     mavlink_status_t *r_mavlink_status) {
+  uint8_t msg_received =
+      mavlink_frame_char_buffer(&mavlink_message_rx_buffer, &mavlink_status, c,
+                                r_message, r_mavlink_status);
+  if (msg_received == MAVLINK_FRAMING_BAD_CRC) {
+    LOG_ERROR("MAVLink message parse error: Bad CRC");
+  } else if (msg_received == MAVLINK_FRAMING_BAD_SIGNATURE) {
+    LOG_ERROR("MAVLink message parse error: Bad signature");
+  }
+
+  return msg_received;
+}
+
+static void handle_char(uint8_t c) {
+  // ZF_LOGE("%02X", c);
+  mavlink_message_t msg;
+  mavlink_status_t status;
+  int result = my_mavlink_parse_char(c, &msg, &status);
+  uint8_t buf[300];
+  uint32_t len;
+
+  if (result) {
+    LOG_ERROR("Message: [SEQ]: %d, [MSGID]: 0x%06X, [SYSID]: %d, [COMPID]: %d",
+              msg.seq, msg.msgid, msg.sysid, msg.compid);
+
+    len = mavlink_msg_to_send_buffer(buf, &msg);
+    serial_send(buf, len);
+  }
+}
+
+int run(void) {
+  ZF_LOGE("In run");
+
+  ring_buffer_t *ringbuffer = (ring_buffer_t *)ring_buffer;
+  uint32_t head, tail;
+  head = ringbuffer->head;
+  ring_buffer_acquire();
+
+  while (1) {
+    tail = ringbuffer->tail;
+    ring_buffer_acquire();
+    while (head != tail) {
+      handle_char(ringbuffer->buffer[head]);
+      ring_buffer_acquire();
+      head = (head + 1) % sizeof(ringbuffer->buffer);
+    }
+  }
+  return 0;
+}
